35aebb8abd39907c82b29f0084a625b38adabe03
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-11-01 23:51:12 +0700

    Start React

--- 3 - NODE-JS/HTML-CSS-JS/index.js
+++ 3 - NODE-JS/HTML-CSS-JS/index.js
@@ -1,8 +1,8 @@
 function changeColor() {
 
-    let tcolor = document.querySelector('#inputone').value
-    let bcolor = document.querySelector('#inputtwo').value
+    let tcolor = document.querySelector('#inputone').value;
+    let bcolor = document.querySelector('#inputtwo').value;
 
-    document.querySelector('h2').style.color = tcolor
-    document.querySelector('h2').style.backgroundColor = bcolor
+    document.querySelector('h2').style.color = tcolor;
+    document.querySelector('h2').style.backgroundColor = bcolor;
 }
